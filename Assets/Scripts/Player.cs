791d5dec8dd90e2377fb66dd7e9b507e77c5752f
Anthony Briscoe <tony.briscoe1@gmail.com>
2018-09-03 20:00:34 +0100

    Added horizontal movement to player

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -4,6 +4,7 @@ using UnityEngine;
 
 public class Player : MonoBehaviour {
 
+	private float _speed = 3.5f;
 	// Use this for initialization
 	void Start () {
 		
@@ -11,6 +12,9 @@ public class Player : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-		
+		float horizontalInput = Input.GetAxis("Horizontal");
+		Vector3 direction = new Vector3(horizontalInput, 0, 0);
+		Vector3 velocity = direction * _speed;
+		transform.Translate(velocity * Time.deltaTime);
 	}
 }
